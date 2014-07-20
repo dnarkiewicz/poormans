c99014b707c29e2a6dc8af25c403ed6a3bae6d20
dan narkiewicz <dnarkiewicz@ctacorp.com>
2014-07-20 05:27:30 +0000

    moved settings to config/config.js, added dropbox account info pull

---
+++ config/config-default.js
@@ -0,0 +1,15 @@
+var config = {}
+
+config.dir  = '/Dropbox/Apps/Poormans/';
+config.file = 'poormans.md';
+config.port = 1234;
+
+config.logLevel = 'info';
+
+config.dropbox = {
+    key    : '',
+    secret : '',
+    token  : ''
+};
+
+module.exports = config;
